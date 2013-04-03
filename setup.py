451963accfb53616cb7941379b00b7929bc32544
Branko Vukelic <branko@brankovukelic.com>
2013-04-04 01:36:16 +0200

    Updated manifest and setup module

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='django-meta',
+    description='Pluggable app for handling webpage metadata and META tags',
+    long_description=open('README.rst').read(),
+    version='0.0.1',
+    packages=['meta', 'meta.templatetags'],
+    package_data={
+        'meta': ['templates/*.html'],
+    },
+    author='Monwara LLC',
+    author_email='branko@monwara.com',
+    url='https://bitbucket.org/monwara/django-meta',
+    download_url='https://bitbucket.org/monwara/django-meta/downloads',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
+
