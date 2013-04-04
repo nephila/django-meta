050cdf705601d27311f259e52e9372769145280a
Branko Vukelic <branko@brankovukelic.com>
2013-04-04 14:41:50 +0200

    Added new settings

--- meta/settings.py
+++ meta/settings.py
@@ -1,7 +1,11 @@
 from django.conf import settings
 
+SITE_PROTOCOL = getattr(settings, 'META_SITE_PROTOCOL', None)
+SITE_DOMAIN = getattr(settings, 'META_SITE_DOMAIN', None)
 SITE_TYPE = getattr(settings, 'META_SITE_TYPE', None)
 SITE_NAME = getattr(settings, 'META_SITE_NAME', None)
-SITE_PROTOCOL = getattr(settings, 'META_SITE_PROTOCOL', 'http')
-SITE_DOMAIN = getattr(settings, 'META_SITE_DOMAIN', None)
+INCLUDE_KEYWORDS = getattr(settings, 'META_INCLUDE_KEYWORDS', [])
+DEFAULT_KEYWORDS = getattr(settings, 'META_DEFAULT_KEYWORDS', [])
+IMAGE_URL = getattr(settings, 'META_IMAGE_URL', settings.STATIC_URL)
 USE_OG_PROPERTIES = getattr(settings, 'META_USE_OG_PROPERTIES', False)
+USE_SITES = getattr(settings, 'META_USE_SITES', False)
