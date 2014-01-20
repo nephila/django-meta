790d3df3b90db2e2b5130a00bbbbdd3cc2f7f1b6
Iacopo Spalletti <i.spalletti@nephila.it>
2014-01-20 18:51:42 +0100

    Add two more templatetags for Google+ and for generic OpenGraph tags

--- meta/templatetags/meta.py
+++ meta/templatetags/meta.py
@@ -5,20 +5,41 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
+def generic_prop(namespace, name, value):
+    """
+    Generic property setter that allows to create custom namespaced meta
+    """
+    return '<meta property="%s:%s" content="%s">' % (namespace, name, value)
+
+
+@register.simple_tag
 def og_prop(name, value):
     return '<meta property="og:%s" content="%s">' % (name, value)
+
 
 @register.simple_tag
 def twitter_prop(name, value):
     return '<meta name="twitter:%s" content="%s">' % (name, value)
 
+
 @register.simple_tag
 def googleplus_prop(name, value):
     return '<meta itemprop="%s" content="%s">' % (name, value)
 
+
+@register.simple_tag
+def googleplus_html_scope(value):
+    """
+    This is meant to be used as attribute to html / body or other tags to
+    define schema.org type
+    """
+    return ' itemscope itemtype="http://schema.org/%s" ' % value
+
+
 @register.simple_tag
 def meta(name, value):
     return '<meta name="%s" content="%s">' % (name, value)
+
 
 @register.simple_tag
 def meta_list(name, lst):
