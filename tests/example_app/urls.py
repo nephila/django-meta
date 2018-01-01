5c34d590068979307af4f989648372188760737d
Iacopo Spalletti <i.spalletti@nephila.it>
2018-01-01 11:13:13 +0100

    Django 2.0 compatibility

--- tests/example_app/urls.py
+++ tests/example_app/urls.py
@@ -1,23 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-import django.views.i18n
 import django.views.static
 from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 from .views import PostDetailView, PostListView, PostMixinDetailView
+
+try:
+    from django.views.i18n import JavaScriptCatalog
+    javascript_catalog = JavaScriptCatalog.as_view()
+except ImportError:
+    from django.views.i18n import javascript_catalog
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', django.views.static.serve,  # NOQA
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'^admin/', include(admin.site.urls)),  # NOQA
-    url(r'^jsi18n/(?P<packages>\S+?)/$', django.views.i18n.javascript_catalog),  # NOQA
+    url(r'^jsi18n/(?P<packages>\S+?)/$', javascript_catalog),  # NOQA
     url(r'^mixin/(?P<slug>\w[-\w]*)/$', PostMixinDetailView.as_view(), name='post-detail-mixinx'),
     url(r'^(?P<slug>\w[-\w]*)/$', PostDetailView.as_view(), name='post-detail'),
     url(r'^$', PostListView.as_view(), name='post-list'),
 ]
+try:
+    urlpatterns.insert(0, url(r'^admin/', admin.site.urls)),  # NOQA
+except Exception:
+    urlpatterns.insert(0, url(r'^admin/', include(admin.site.urls))),  # NOQA
