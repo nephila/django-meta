9fb1fb91ae3a42f7cc97e9088081a560ea5e381c
Peter Morrow <morrow.peter123@gmail.com>
2022-02-28 17:46:00 -0800

    Replaces ugettext_lazy with gettext_lazy
    django.utils.translation.ugettext_lazy ->> django.utils.translation.gettext_lazy

--- tests/example_app/models.py
+++ tests/example_app/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from meta.models import ModelMeta
 
