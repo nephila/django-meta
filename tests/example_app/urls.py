235fa13d235a250c9e7a2813bde582d871fdbb6f
Iacopo Spalletti <i.spalletti@nephila.it>
2023-04-18 23:03:56 +0200

    Move to ruff (#147)
    * Upgrade test urlconf

--- tests/example_app/urls.py
+++ tests/example_app/urls.py
@@ -1,34 +1,19 @@
-import django.views.static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import path
+from django.views.i18n import JavaScriptCatalog
+from django.views.static import serve
 
 from .views import PostDetailView, PostListView, PostMixinDetailView, PostMixinImageObjectDetailView
-
-try:
-    from django.views.i18n import JavaScriptCatalog
-
-    javascript_catalog = JavaScriptCatalog.as_view()
-except ImportError:
-    from django.views.i18n import javascript_catalog
 
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(
-        r"^media/(?P<path>.*)$",
-        django.views.static.serve,  # NOQA
-        {"document_root": settings.MEDIA_ROOT, "show_indexes": True},
-    ),
-    re_path(r"^jsi18n/(?P<packages>\S+?)/$", javascript_catalog),  # NOQA
-    re_path(r"^mixin/(?P<slug>\w[-\w]*)/$", PostMixinDetailView.as_view(), name="post-detail-mixinx"),
-    re_path(
-        r"^mixin_image/(?P<slug>\w[-\w]*)/$", PostMixinImageObjectDetailView.as_view(), name="post-detail-image-mixinx"
-    ),
-    re_path(r"^(?P<slug>\w[-\w]*)/$", PostDetailView.as_view(), name="post-detail"),
-    re_path(r"^$", PostListView.as_view(), name="post-list"),
+    path("admin/", admin.site.urls),
+    path("media/<str:path>", serve, {"document_root": settings.MEDIA_ROOT, "show_indexes": True}),
+    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
+    path("mixin/<slug:slug>/", PostMixinDetailView.as_view(), name="post-detail-mixinx"),
+    path("mixin_image/<slug:slug>/", PostMixinImageObjectDetailView.as_view(), name="post-detail-image-mixinx"),
+    path("<slug:slug>/", PostDetailView.as_view(), name="post-detail"),
+    path("", PostListView.as_view(), name="post-list"),
 ]
-try:
-    urlpatterns.insert(0, re_path(r"^admin/", admin.site.urls)),  # NOQA
-except Exception:
-    urlpatterns.insert(0, re_path(r"^admin/", include(admin.site.urls))),  # NOQA
