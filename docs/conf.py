b402effbe3c7ad242be4b1e3985d0be21a9c1913
Leonardo Cavallucci <leo.cavallucci@gmail.com>
2025-01-14 16:11:14 +0100

    Fix docs build (#214)

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,6 @@
 import os
 import sys
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-import sphinx_rtd_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -117,8 +113,6 @@ pygments_style = "sphinx"
 
 
 html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
